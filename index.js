b3325facb5b402d84916dd0bea22e0654b60ec79
DidasMba <daniel.mbarushi@gmail.com>
2023-10-17 08:22:24 +0300

    add ul with js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const element = document.createElement("div");
 document.body.append(element);
 
-document.querySelector('main#main').remove();
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+
+//document.querySelector('main#main').remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
